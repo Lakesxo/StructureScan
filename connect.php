9d59753c1cd7d193652ce4b597b747323d0c361d
Lakes <olaksajanaku@yahoo.com>
2020-07-16 15:40:34 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,15 +2,15 @@
     $email = $_POST['email'];
 
     // Database connection
-    $conn = new mysqli('localhost','root','','structure_scan');
+    $conn = new mysqli('localhost','id14332246_structuremails','@Rexiolar02#','id14332246_structure_scan');
     if($conn->connect_error){
         die('Connection Failed : '.$conn->connect_error);
     }else{
         $stmt = $conn->prepare("insert into emails(email) values(?)");
         $stmt->bind_param("s",$email);
         $stmt->execute();
-        //  To redirect form on a particular page
-        header("Location: http://localhost/StructureScan/#sub_sec");
+        //  To redirect form to landing page
+        header("Location: https://structurescan.000webhostapp.com/");
         die;
         // echo "Thanks for subscribing";
         $stmt->close();
